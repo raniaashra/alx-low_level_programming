8935aa19b284808dd2c8c87b3e80797b97be9324
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 15:24:05 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,9 @@ int main(void)
 {
 int d;
 char low;
-for (d = '0'; d <='9'; d++)
+for (d = '0'; d <= '9'; d++)
 putchar(d);
-for ( low = 'a'; low <= 'f'; low++)
+for (low = 'a'; low <= 'f'; low++)
 putchar(low);
 putchar('\n');
 return (0);
