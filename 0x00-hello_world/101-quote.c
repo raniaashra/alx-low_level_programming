713a63bb896b43d65df824eaefb2da31261b9a5d
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 11:39:09 -0700

    101-quote.c2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - function print line without printf and puts function
  * Return: 0
  */
 int main(void)
 {
-cout<< "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
-return (0);
+fput("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",stdout);
+return (1);
 }
