0dbb9ef2dfdf5005f32e649899cdc0bcb83c8270
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 15:10:29 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - letter in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch--;
+putchar(ch);
+putchar('\n');
+return (0);
+}
