54a56dbc30353ce9fff5be422409f4b49c983e4c
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 10:17:04 -0700

    4-puts.c7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+ * main function to print the line in function
+ * return 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return 0;
 }
 
