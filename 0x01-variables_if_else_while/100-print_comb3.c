f58f71df8789a445b793ac9023bb80cc65f935ce
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 15:52:38 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print combination og digit
+ * Return: Always 0
+ */
+int main(void)
+{
+int d, k;
+for (d = '0'; d < '9'; d++)
+{
+for (k = d+1; k <= '9'; k++)
+{
+if (k != d)
+{
+putchar(d);
+putchar(k);
+if (d == '0' && k == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
