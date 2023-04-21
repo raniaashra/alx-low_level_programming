a43650972d014bfebed45da2d8b7d64757d28b1a
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 11:00:40 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print size of the variables type
+ * Return: 0
+ */
+int main(void)
+{
+printf("%lu\n",sizeof(char));
+printf("%lu\n",sizeof(int));
+printf("%lu\n",sizeof(long int));
+printf("%lu\n",sizeof(long long int));
+printf("%lu\n",sizeof(float));
+return (0);
+}
