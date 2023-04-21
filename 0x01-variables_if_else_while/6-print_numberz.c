5004a312fdc7e6efe0434e9276a2494866677d65
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 15:05:00 -0700

    6-print_numberz.c3

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
  * main - print all digit from 0
- * REturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int x;
-
-    for (x = 0; x < 10; x++)
-    {
-        putchar(x + '0');
-     }
-    putchar('\n');
-    return (0);
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0');
+}
+putchar('\n');
+return (0);
 }
