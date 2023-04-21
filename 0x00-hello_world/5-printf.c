5f42130b7647b755e4718fa63a80453b356ee3c0
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 10:48:58 -0700

    5-printf.c3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - use printf to print the line
  * Return: 0
  */
-int main()
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
