94a3f7e12428cb47fff221e957d99e1172cf5b8c
Rania ashraf <raniaashraf307@gmail.com>
2023-04-21 14:57:50 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print first ten digit of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0');
+}
+putchar('\n');
+return (0);
+}
