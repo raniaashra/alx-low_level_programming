89d72c83db8caf560ad878014a06fd4c07f521b7
Rania ashraf <raniaashraf307@gmail.com>
2023-04-25 15:16:38 -0700

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
  * main - first 10 items fibonacci
+ * less
  * Return: ...
  */
 int main(void)
 {
-	int x = 0;
-	long y = 1, z = 2, sum = z;
-	while (z + y < 4000000)
-	{
-		z += y;
-		if (z % 2 == 0)
-			sum += z;
-		y = z - y;
-		++x;
-	}
-	printf("%ld\n", sum);
-	return (0);
+int x = 0;
+long y = 1, z = 2, sum = z;
+while (z + y < 4000000)
+{
+z += y;
+if (z % 2 == 0)
+sum += z;
+y = z - y;
+++x;
+}
+printf("%ld\n", sum);
+return (0);
 }
