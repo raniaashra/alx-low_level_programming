28cb814b66732fbef54273735e1c7abf8312c40e
Rania ashraf <raniaashraf307@gmail.com>
2023-04-25 15:13:04 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - first 10 items fibonacci
+ * Return: ...
+ */
+int main(void)
+{
+	int x = 0;
+	long y = 1, z = 2, sum = z;
+	while (z + y < 4000000)
+	{
+		z += y;
+		if ( z % 2 == 0)
+			sum += z;
+		y = z - y;
+		++x;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
