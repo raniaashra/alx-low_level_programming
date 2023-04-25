fcf49b27a22860db1d3299b6c9437dc85231cf35
Rania ashraf <raniaashraf307@gmail.com>
2023-04-25 12:29:44 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - chaecks alphabetic 
+ * @c: check if is alpha
+ * Return: 1 if letter is lower and upper or 0 if other
+ */
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || ( c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
