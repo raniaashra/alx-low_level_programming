00551429faf3cfff82cf97dd2ddee0e2fb78f11c
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 15:59:12 -0700

    5-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_line - draw straight line
+ * Return: no
+ */
+void print_line(int n)
+{
+int x;
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+putchar('_');
+}
+putchar('\n');
+}
+}
