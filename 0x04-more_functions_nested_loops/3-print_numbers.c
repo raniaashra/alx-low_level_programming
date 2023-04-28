609992ccfe0466ca72bca7d8ece8a2d0dbb2a752
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 14:52:30 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
+char i;
+for (i = '0'; i < '10'; i++)
 {
-_putchar(i+ '0');
+_putchar(i);
 }
-_putchar("\n");
+_putchar('\n');
 }
