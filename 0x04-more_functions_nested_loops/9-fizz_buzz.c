c792989cf3325a8772ffa40325295c6ee99ac460
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 16:46:12 -0700

    9-fiaa_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int x;
-for (x = 1; x < 100; x++)
+for (x = 1; x <= 100; x++)
 {
 if (x % 3 == 0 && x % 5 == 0)
 {
@@ -24,7 +24,7 @@ else
 {
 printf("%d", x);
 }
-if ( x!= 100)
+if (x != 100)
 {
 printf(" ");
 }
