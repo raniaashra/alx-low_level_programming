17332fa4e98a4e3b93c08e9a31af3432abe7c8b6
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 15:37:52 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * more_numbers - from 0 to 14
+ * Return: no
+ */
+void more_numbers(void)
+{
+int c;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (c = 0; c <= 14; c++)
+{
+if (c > 0)
+{
+putchar((c / 10) + '0');
+}
+putchar((c % 10) + '0');
+}
+putchar('\n');
+}
+}
