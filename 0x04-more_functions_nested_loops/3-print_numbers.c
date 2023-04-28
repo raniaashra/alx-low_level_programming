1a209dd8778446b32091768d7c8d15eb50afe86b
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 14:29:03 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: Success
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i+ '0');
+}
+_putchar("\n");
+}
