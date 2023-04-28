8710be3f27517a3815d75e19278bbb7020e433f6
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 13:53:49 -0700

    0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isupper - return character in upper case
+ * @c: value of character
+ * Return: 1 if c is upper or 0 if otherwise
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+return (0);
+}
--- 0-main.c
+++ 0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
 }
