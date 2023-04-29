55495c2c5f800c33184609c2ff31cb134332b804
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 17:28:28 -0700

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - prime factor
+ * Return: 0
+ */
+int main(void)
+{
+long x, max;
+long number = 1231952;
+double square = sqrt(number);
+for (x = 1; x <= square; x++)
+{
+if (number % x == 0)
+{
+max = number / x;
+}
+}
+printf("%ld\n", max);
+return (0);
+}
