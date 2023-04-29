4afc86be788ba2d65a13ce99e516b8f7a0d4d1d0
Rania ashraf <raniaashraf307@gmail.com>
2023-04-28 17:57:51 -0700

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,10 @@ void print_number(int n)
 unsigned int k = n;
 if (n < 0)
 {
-n *= -1;
-k = n;
 putchar('-');
+k = -k;
 }
-k /= 10;
-if (k != 0)
-print_number(k);
-putchar((unsigned int) n % 10 + '0');
+if ((k / 10) > 0)
+print_number(k / 10);
+putchar(( k % 10) + '0');
 }
