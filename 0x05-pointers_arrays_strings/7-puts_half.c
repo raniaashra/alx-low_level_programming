f995795af8dd3889b0f915f4e52cccb5f8fe472d
Rania ashraf <raniaashraf307@gmail.com>
2023-05-02 17:32:07 -0700

    half_number

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * puts_half - print half of string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int k;
+while (str[i] != '\0')
+{
+i++;
+}
+if ( i % 2 == 1)
+{
+k = (i - 1) / 2;
+k += 1;
+}
+else
+{
+k = i / 2;
+}
+for (; k < i; k++)
+{
+putchar(str[k]);
+}
+putchar('\n');
+}
