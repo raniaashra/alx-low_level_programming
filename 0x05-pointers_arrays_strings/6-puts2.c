9524d3de1edf9082ff65c490a1121883b573053b
Rania ashraf <raniaashraf307@gmail.com>
2023-05-02 17:13:59 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * puts2 - print every character
+ * @str: string
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i = 0;
+int len = 0;
+char *k = str;
+int l;
+while (*k != '\0')
+{
+i++;
+k++;
+}
+len = i - 1;
+for (l = 0; l <= lenl l++)
+{
+if (l % 2 == 0)
+{
+putchar(str[l]);
+}
+}
+putchar('\n');
+}
