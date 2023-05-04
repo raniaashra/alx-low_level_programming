bfdc9a4c6406404e362789bdccebeb62e8bc4ea9
Rania ashraf <raniaashraf307@gmail.com>
2023-05-04 16:40:26 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  *_strcat - append src string 
+ *@dest: char type string
+ *@src: char type string
+ *Return: pointer 
+*/
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
