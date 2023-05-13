76f494531d6c664fd7690daef0af1d5cc670a212
Rania ashraf <raniaashraf307@gmail.com>
2023-05-12 18:41:58 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print len
+ * @argc: int
+ * @argv: char
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
