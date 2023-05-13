96cee22f88623f487db1fa8083704888bb75d444
Rania ashraf <raniaashraf307@gmail.com>
2023-05-12 18:48:50 -0700

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,12 @@
  * @argv:char
  * Return: always 0
  */
-int main(int argc, char argv)
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0;i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
-return (0)
+return (0);
 }
