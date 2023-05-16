eb6af7f2a8b6d966ad8a0a19ec95f5fcb35e938f
Rania ashraf <raniaashraf307@gmail.com>
2023-05-16 15:58:00 -0700

    duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup = dublicate string
+ * @str: string
+ * Return: the string dublicate
+ */
+char *_strdup(char *str)
+{
+int a = 0, i = 1;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[i])
+{
+i++;
+}
+s = malloc((sizeof(char) * i) + i);
+if (s == NULL)
+return (NULL);
+while (a < i)
+{
+s[a] = str[a];
+a++;
+}
+s[a] = '\0';
+return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *create_array(unsigned int i, char c);
+char *_strdup(char *str);
 #endif
